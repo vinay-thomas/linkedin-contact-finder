6d93990bf347dffab00064a294bc4d5c46e43176
Vinay Thomas <vinayiype@gmail.com>
2021-11-01 19:57:59 -0400

    Finalized script

--- main.py
+++ main.py
@@ -7,16 +7,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import csv
 import time
+import os
+import subprocess, os, platform
+from dotenv import load_dotenv
+load_dotenv()
+
+companies = os.getenv('COMPANIES').split(",")
+formats = os.getenv('FORMATS').split(",")
+username = os.getenv('LINKEDIN_USERNAME')
+password = os.getenv('LINKEDIN_PASSWORD')
+filename = "contacts.csv"
 
 driver = webdriver.Chrome()
-
-
+        
+# Log into LinkedIn
 def log_into_linked_in():
     driver.get("https://www.linkedin.com/login")
     email_field = driver.find_element(By.XPATH, '//*[@id="username"]')
-    email_field.send_keys("thomas.vin@husky.neu.edu")
+    email_field.send_keys(username)
     password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
-    password_field.send_keys("Mario1023!")
+    password_field.send_keys(password)
     driver.find_element(
         By.XPATH, '//*[@id="organic-div"]/form/div[3]/button').click()
     try:
@@ -24,24 +34,48 @@ def log_into_linked_in():
     except:
         return
 
-
+# Append string to url
 def append_to_url(str):
     url = driver.current_url
     newurl = url+str
     driver.get(newurl)
 
 # TODO
-def get_email_of(person):
-    return 'email@email.com'
+# Read email format to get email of person
+def get_email_of(person, company):
+    company_idx = companies.index(company)
+    unformatted_email = formats[company_idx]
+    email = format_email(unformatted_email, person)
+    return email
 
+def format_email(format, person):
+    name = get_name_of(person).lower()
+    names = name.split(" ")
+    first_name = names[0]
+    last_name = names[len(names)-1]
 
+    if "first" in format:
+        format = format.replace("first",first_name)
+    else:
+        first_initial = first_name[0]
+        format = format.replace("f", first_initial)
+    
+    if "last" in format:
+        format = format.replace("last",last_name)
+    else:
+        last_initial = last_name[0]
+        format = format.replace("l",last_initial)
+
+    return format
+
+# Get LinkedIn link of person
 def get_link_of(person):
     link = person.find_elements(By.XPATH, './/a')
     if len(link) > 0:
         return link[0].get_attribute("href")
     return None
 
-
+# Get role of person
 def get_role_of(person):
     role = person.find_elements(
         By.XPATH, './section/div/div/div[2]/div[3]/div/div')
@@ -49,7 +83,7 @@ def get_role_of(person):
         return role[0].text
     return None
 
-
+# Get name of person
 def get_name_of(person):
     name = person.find_elements(
         By.XPATH, './section/div/div/div[2]/div[1]/a/div')
@@ -58,15 +92,15 @@ def get_name_of(person):
     return None
 
 
-accepted_roles = ['analyst', 'associate', 'investment manager']
-excluded_roles = ['director', 'officer', 'chief']
+accepted_roles = ['analyst', 'associate', 'investment manager', 'portfolio manager']
+excluded_roles = ['director', 'officer', 'chief', 'incoming', 'partner', 'intern', 'quantitative']
 
-
+# boolean : if current role is in accepted roles and exclude excluded roles
 def filter_by_role(person):
     role = get_role_of(person)
     return role != None and any(x in role.lower() for x in accepted_roles) and not any(x in role.lower() for x in excluded_roles)
 
-
+# Scrolls all the way down LinkedIn 
 def get_all_people():
     previous_people = driver.find_elements(
         By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div/div[1]/ul/li')
@@ -78,16 +112,17 @@ def get_all_people():
         time.sleep(3)
         people = driver.find_elements(
             By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div/div[1]/ul/li')
-    return people
+        relevant_people = list(filter(filter_by_role,people))
+        if(len(relevant_people) > 0):
+            return list(filter(filter_by_role,people))
+    return None
 
-
+# Records Organization, Name, Role, LinkedIn Link, and EMail to CSV
 def write_all_people_to_csv(people):
     # field names
     fields = ['Organization', 'Contact',
               'Position/Title', 'LinkedIn', 'E-Mail']
 
-    # name of csv file
-    filename = "contacts.csv"
 
     # writing to csv file
     with open(filename, 'w', newline='') as csvfile:
@@ -100,15 +135,15 @@ def write_all_people_to_csv(people):
         # writing the data rows
         csvwriter.writerows(people)
 
-
+# Gives a list of all of a person's attributes
 def map_person_to_row(person, company):
     name = get_name_of(person)
     role = get_role_of(person)
     link = get_link_of(person)
-    email = get_email_of(person)
+    email = get_email_of(person, company)
     return [company, name, role, link, email]
 
-
+# Searches LinkedIn for a given company and goes to the "People" tab
 def search(company):
     driver.get("https://www.linkedin.com/search/results/companies/?keywords=" +
                urllib.parse.quote(company))
@@ -125,14 +160,23 @@ def search(company):
                 return map_person_to_row(people[0], company)
     except:
         pass
-    print("===============Company '" + company +
+    print("===============Company '" + company +    
             "' was not found=======================")
     return None
 
+def open_csv():
+    if platform.system() == 'Darwin':       # macOS
+        subprocess.call(('open', filename))
+    elif platform.system() == 'Windows':    # Windows
+        os.startfile(filename)
+    else:                                   # linux variants
+        subprocess.call(('xdg-open', filename))
 
 log_into_linked_in()
-companies = ["MIT Investment Management Company",
-             "Ireland Strategic Investment Fund", "Argentum Asset Management"]
 people_of_interest = list(
-    filter((lambda x: x != None), list(map((lambda x: search(x)), companies))))
+    filter((lambda x: x != None), list(
+        map((lambda x: search(x)), companies))))
 write_all_people_to_csv(people_of_interest)
+driver.close()
+driver.quit()
+open_csv()
